f17f38850043e556dd459ac4566a34e2c2ab9e0b
ajay <mohit uniyal@gmail.com>
2025-05-18 08:24:09 +0530

    updated table generator

--- GenerateTable/index.js
+++ GenerateTable/index.js
@@ -1,15 +1,25 @@
 function generateTable(e) {
   e.preventDefault();
 
-  const rows = parseInt(document.getElementById("rows").value);
-  const cols = parseInt(document.getElementById("cols").value);
-
-  console.log("rows", rows + "and cols", cols);
+  const rowVal = parseInt(document.getElementById("rows").value);
+  const colVal = parseInt(document.getElementById("cols").value);
+  const container = document.getElementById("tableContainer");
+  console.log("rows", rowVal + "and cols", colVal);
+  let table = "<table>";
+  let count = 1;
+  for (let i = 0; i < rowVal; i++) {
+    let row = [];
+    for (let j = 0; j < colVal; j++) {
+      row.push(count++);
+    }
+    if (i % 2 !== 0) row.reverse();
+    let rowData = "<tr>";
+    for (let k = 0; k < row.length; k++) {
+      rowData += "<td>" + row[k] + "</td>";
+    }
+    rowData += "</tr>";
+    table += rowData;
+  }
+  table += "</table";
+  container.innerHTML = table;
 }
-
-/*document.getElementById("zigzagform").addEventListener("submit", function (e) {
-  e.preventDefault();
-  const rows = parseInt(document.getElementById("rows").value);
-  const cols = parseInt(document.getElementById("cols").value);
-  console.log("rows", rows + "and cols", cols);
-});*/
